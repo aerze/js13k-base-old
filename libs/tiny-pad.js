e40ef33ed3c97f07df9835bca885ea3836b1d153
Gus Suarez <gus@origincodeacademy.com>
2017-06-16 05:04:40 -0700

    minor changes in tiny-pad

--- libs/tiny-pad.js
+++ libs/tiny-pad.js
@@ -9,52 +9,48 @@ const BUTTONS = {
   B: 8
 }
 
-class SimplePad {
-  constructor () {
-    this.isDown = {}
-    this.prevDown = {}
+function update (pad, gp) {
+  if (gp) {
+    const y = gp.axes[1] << 0
+    const x = gp.axes[0] << 0
+    _set(pad, BUTTONS.UP, gp.axes[1] = y < 0)
+    _set(pad, BUTTONS.DOWN, gp.axes[1] = y > 0)
+    _set(pad, BUTTONS.LEFT, gp.axes[0] = x < 0)
+    _set(pad, BUTTONS.RIGHT, gp.axes[0] = x > 0)
+    _set(pad, BUTTONS.START, gp.buttons[8].value > 0)
+    _set(pad, BUTTONS.SELECT, gp.buttons[9].value > 0)
+    _set(pad, BUTTONS.A, gp.buttons[1].value > 0)
+    _set(pad, BUTTONS.B, gp.buttons[2].value > 0)
   }
+}
 
-  update (gp) {
-    if (gp) {
-      const y = gp.axes[1] << 0
-      const x = gp.axes[0] << 0
-      this._set(BUTTONS.UP, gp.axes[1] = y < 0)
-      this._set(BUTTONS.DOWN, gp.axes[1] = y > 0)
-      this._set(BUTTONS.LEFT, gp.axes[0] = x < 0)
-      this._set(BUTTONS.RIGHT, gp.axes[0] = x > 0)
-      this._set(BUTTONS.START, gp.buttons[8].value > 0)
-      this._set(BUTTONS.SELECT, gp.buttons[9].value > 0)
-      this._set(BUTTONS.A, gp.buttons[1].value > 0)
-      this._set(BUTTONS.B, gp.buttons[2].value > 0)
-    }
-  }
+function _set (pad, k, bool) {
+  if (pad.isDown[k] !== pad.prevDown[k]) pad.prevDown[k] = pad.isDown[k]
+  pad.isDown[k] = bool
+}
 
-  _set (k, bool) {
-    if (this.isDown[k] !== this.prevDown[k]) this.prevDown[k] = this.isDown[k]
-    this.isDown[k] = bool
-  }
-
+const Pad = {
+  isDown: {},
+  prevDown: {},
   justPressed (key) {
     return this.isDown[key] && !this.prevDown[key]
-  }
-
+  },
   justReleased (key) {
     return !this.isDown[key] && this.prevDown[key]
   }
 }
 
 const TinyPad = [
-  new SimplePad(),
-  new SimplePad(),
-  new SimplePad(),
-  new SimplePad()
+  Object.create(Pad),
+  Object.create(Pad),
+  Object.create(Pad),
+  Object.create(Pad)
 ]
 
 TinyPad.update = function () {
   const gps = navigator.getGamepads()
   this.forEach(function (pad, i) {
-    pad.update(gps[i])
+    update(pad, gps[i])
   })
 }
 
